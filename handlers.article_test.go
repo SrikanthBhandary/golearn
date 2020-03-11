9d46822dea248de7436c55c8507af437514fd919
SrikanthBhandary <srikanth.bhandary@gmail.com>
2020-03-11 15:48:09 +0530

    Add files via upload

---
+++ handlers.article_test.go
@@ -0,0 +1,20 @@
+package main
+
+import "testing"
+import "net/http"
+import "net/http/httptest"
+import "strings"
+import "io/ioutil"
+
+func TestShowIndexPageUnauthenticated(t *testing.T){
+	r := getRouter(true)
+	r.GET("/", showIndexPage)
+	req, _ := http.NewRequest("GET", "/", nil)
+	testHTTPResponse(t, r, req, func(w *httptest.ResponseRecorder) bool{
+		statusOK := w.Code == http.StatusOK
+		p, err := ioutil.ReadAll(w.Body)
+		pageOK := err == nil && strings.Index(string(p), "<title>Home Page</title>") > 0
+		return statusOK && pageOK
+	})
+
+}
